c7a1c3b3d42d03788a1a4440b1197e13891f1873
Jim Fisher <jim@b4auctions.com>
2026-08-31 06:58:13 -0500

    Configure Vite root to src and outDir to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
+  root: './src', // Tells Vite that index.html is located in src/
   build: {
-    outDir: '../dist',
-    minify: false,
+    outDir: '../dist', // Places the built assets into AutoManageVM/dist
     emptyOutDir: true,
   },
 });
